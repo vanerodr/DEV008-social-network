84d672acd5111a0387a04ac03f1b62cc9844b3bc
Mahanaim <castromahanaim@gmail.com>
2023-07-20 12:00:27 -0600

    routes

---
+++ src/inicioSesion.js
@@ -0,0 +1,11 @@
+import logo from "./imagenes/elq.png"
+export default () => {
+  const viewiniciodeSesion = `
+    <h2 class="tex-center">Inicio de sesión </h2>
+    <figure>
+    <img src="${logo}">
+    </figure>`;
+  const divElem = document.createElement('div');
+  divElem.innerHTML = viewiniciodeSesion;
+  return divElem;
+};
