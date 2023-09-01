a2f54d3ceee1f149ee109dce2e2b710c242f3047
Mahanaim <castromahanaim@gmail.com>
2023-09-01 09:39:08 -0600

    muro Comportamiento

--- src/vistas/muro.js
+++ src/vistas/muro.js
@@ -1,10 +1,14 @@
 import { crearPublicacion } from '../init-firebase';
 import { juntarPublis } from '../init-firebase';
+//import { eliminarPublicacion } from '../init-firebase';
 
 export const muro = () => {
   const viewMuro = document.createElement('div');
   viewMuro.innerHTML = `
+  <figure ><img class="fondoMuro" src="https://miro.medium.com/v2/resize:fit:1000/1*6zrUW8rgLSPjdMXFQyk9CQ.jpeg">
+  <h2 class= "<tituloRed">LabsConnect</h2>
   <div class="container p-4">
+
     <div class="row">
       <div class="col-md-6">
       </div>
@@ -28,6 +32,7 @@ export const muro = () => {
                 </textarea>
               </div>
               <button type="submit" class="btnMuro" id="btnForma"> Publicar
+             
             </form>
           </div>
         </div>
@@ -62,16 +67,35 @@ export function muroComportamiento() {
     });
     console.log('Lista para conectar');
   });
+    // eslint-disable-next-line no-shadow
+    //eliminarPublicacion.addEventListener('click', (e) => {
+      //e.preventDefault();
+      //eliminarPublicacion(actividad, descripcion).then(() => {
+        //console.log('50%');
+      //}).catch(() => {
+        //console.log('error');
+      //});
+      //console.log('100%');
+    //});
   juntarPublis().then((publis) => {
     console.log('correcto');
     publis.forEach((doc) => {
+      const actividadPublicacion = document.createElement('div');
+      actividadPublicacion.classList.add('div');
+      contenedorPublicaciones.appendChild(actividadPublicacion);
       const publi = document.createElement('p');
       publi.classList.add('publi');
       publi.textContent = doc.data().publicacion;
-      contenedorPublicaciones.appendChild(publi);
-      // doc.data() is never undefined for query doc snapshots
-      console.log(doc.id, " => ", doc.data());
-    });
+      actividadPublicacion.appendChild(publi);
+      const actividad = document.createElement('p');
+      actividad.classList.add('actividad');
+      actividad.textContent = doc.data().actividad;
+      actividadPublicacion.appendChild(actividad);
+      /*const eliminarBoton = document.createElement('eliminarBoton');
+      eliminarBoton.classList.add('eliminarBoton');
+      contenedorPublicaciones.appendChild(eliminarBoton);
+      console.log(doc.id, ' => ', doc.data());
+    */});
   }).catch((error) => {
     console.log(error);
   });
